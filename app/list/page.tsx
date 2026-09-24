37f9c4452cb01fd8154b7ba5d9451998464685e3
saho <enabasket.251@gmail.com>
2026-09-24 15:01:01 +0900

    add: 一覧画面の画像表示機能を追加

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import type { WordsListResponse } from "@/app/api/words/route";
-import type { RegisterStoryRequest } from "@/app/api/stories/route";
 import styles from "./page.module.css";
 
 type Tab = "story" | "word";
@@ -23,6 +22,7 @@ type Story={
   id:number;
   title:string;
   content:string;
+  imageUrl?: string | null;
 };
 
 export default function Tabs() {
@@ -41,17 +41,13 @@ export default function Tabs() {
     async function getData() {
       try{
         const response = await fetch("/api/words?userId=1");
-        const responseImage= await fetch("api/stories");
 
         if (!response.ok) {
           throw new Error("データの取得に失敗しました");
         }
-        // if(!responseImage.ok){
-        //   throw new Error("画像の取得に失敗しました");
-        // }
 
         const data: WordsListResponse = await response.json();
-        // const dataImage=
+
 
         //同じ英単語の意味を配列に保持
         const groupedWords = data.words.reduce<GroupedWord[]>((result, word) => {
@@ -75,11 +71,7 @@ export default function Tabs() {
         //物語データの取得
         setStories(data.stories);
       } catch (error) {
-        if (error instanceof Error) {
-          alert(error.message);
-        } else {
-          alert("予期しないエラーが発生しました");
-        }
+        alert("予期しないエラーが発生しました");
       } finally {
         // ローディング終了
         setIsLoading(false);
@@ -141,13 +133,23 @@ export default function Tabs() {
             ) : (
             filteredStories.map((story) => (
               <div key={story.id}className={`${styles.contentDisplay} ${styles.storyContainer}`} >
-                <Link href={`/list/${story.id}`} className={styles.contentDisplay}>
+                <Link href={`/list/${story.id}`}>
                   <div>
-                    <h3 className={styles.title}>{story.title}</h3>  
-                    <img src="" alt="物語のイメージ画像" />  
+                    <div className={styles.storyTitle}>
+                      <h3 className={styles.title}>{story.title}</h3>  
+                      <h3 className={styles.titleLink}>＞</h3>
+                    </div>
+                    <div className={styles.storyDetail}>
+                      {story.imageUrl && (
+                      <img
+                        className={styles.storyImage}
+                        src={story.imageUrl}
+                        alt="物語のイメージ画像"
+                      />
+                    )} 
                     <p className={styles.storyText}>{story.content}</p>
+                    </div>
                   </div>
-                  <h3 className={styles.titleLink}>＞</h3>
                  </Link>
               </div>
             ))
