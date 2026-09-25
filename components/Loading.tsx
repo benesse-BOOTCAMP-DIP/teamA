bf8140b1614711d8a0a5399fa30b0b9dfbf41298
KazzHead <onlybluebookoff@gmail.com>
2026-09-25 16:18:44 +0900

    update: Loading表記

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -5,7 +5,7 @@ import styles from "./Loading.module.css";
 export default function Loading() {
   return (
     <div className={styles.loading}>
-        <p className="radius">読み込み中…</p>
+      <p className="radius">Loading…</p>
     </div>
   );
 }
